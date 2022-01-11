224370154b5168a51d7e385bd7f57dca1acc19e7
IGARASHI Makoto <igarashi@kyokyo-u.ac.jp>
2022-01-11 15:53:19 +0900

    identifyを変える

--- vagrant-vms/copy_wsl_ssh_config.rb
+++ vagrant-vms/copy_wsl_ssh_config.rb
@@ -6,6 +6,10 @@ if $0 == __FILE__
   distrib = 'Ubuntu'
   ssh_hosts = '.ssh/hosts'
 
+  hostname = `hostname`.chomp
+  username = ENV['USERNAME']
+  user_sid = "#{hostname.upcase}\\#{username}"
+
   wsl_home = `wsl -d #{distrib} printenv HOME`.chomp
 
   lin_home_path = "//wsl.localhost/#{distrib}#{wsl_home}"
@@ -19,6 +23,14 @@ if $0 == __FILE__
 
     data = IO.read("#{lin_ssh_hosts}/#{file}")
     data.gsub!(%r{/mnt/([a-z])/}) { $1.upcase + ':/' }
+    if data =~ /^\s+IdentityFile\s+(\S+)\s*$/
+      identity_file = $1
+      if FileTest.file?(identity_file)
+        system('icacls', identity_file, '/grant:r', "#{user_sid}:(F)")
+        system('icacls', identity_file, '/inheritance:r')
+      end
+    end
     IO.write("#{win_ssh_hosts}/#{file}", data)
+    puts file
   end
 end
