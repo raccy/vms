9d8db69cd7f81a75719feeb00790bc0a84212994
raccy <raccy@users.noreply.github.com>
2023-10-07 16:58:43 +0900

    うまくいかない

--- vagrant-vms/wsl/create_wsl.rb
+++ vagrant-vms/wsl/create_wsl.rb
@@ -6,27 +6,39 @@ def install_wsl(distro, user:, password:, version: 2)
   wsl_list = `wsl --list --verbose`
   m = /^\*?\s+#{distro}\s+(\w+)\s+(\d)$/.match(wsl_list)
   if m.nil?
-    Open3.popen2('wsl', '--install', distro) do |stdin, stdout, thr|
-      while (line = stdout.gets)
-        case line
-        when /Enter new UNIX username:/
-          stdin.puts(user)
-          stdin.flush
-        when /New password:/
-          stdin.puts(password)
-          stdin.flush
-        when /Retype new password:/
-          stdin.puts(password)
-          stdin.flush
-        when /\[[^\]]*\]\$/
-          stdin.puts('exit')
-          stdin.flush
-        when /logout/
-          stdin.close
-          stdout.close
-          break
-        end
-      end
+    Open3.popen2('wsl', '--install', distro) do |stdin, stdout, _thr|
+      stdin.puts user
+      stdin.puts password
+      stdin.puts password
+      stdin.puts 'exit'
+      stdin.close
+      print stdout.read
+      # puts 'wsl...'
+      # buff = String.new
+      # loop do
+      #   pp 'loop'
+      #   pp stdout.getc
+      #   stdout.readpartial(1024, buff)
+      #   pp buff
+      #   case buff
+      #   when /Enter new UNIX username:/
+      #     buff.clear
+      #     stdin.puts(user)
+      #     stdin.flush
+      #   when /New password:/, /Retype new password:/
+      #     buff.clear
+      #     stdin.puts(password)
+      #     stdin.flush
+      #   when /\[[^\]]*\]\$/
+      #     buff.clear
+      #     stdin.puts('exit')
+      #     stdin.flush
+      #   when /logout/
+      #     stdin.close
+      #     stdout.close
+      #     break
+      #   end
+      # end
     end
   end
 end
