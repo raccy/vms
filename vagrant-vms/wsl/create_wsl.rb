59f499dbdf037672d2e00177369baa25691072f7
IGARASHI Makoto <igarashi@kyokyo-u.ac.jp>
2023-10-11 15:33:27 +0900

    色々頑張ったのはとりあえず保留

--- vagrant-vms/wsl/create_wsl.rb
+++ vagrant-vms/wsl/create_wsl.rb
@@ -1,82 +1,9 @@
 # create wsl
 
-require 'open3'
-
-def install_wsl(distro, user:, password:, version: 2, timeout: 9999999)
-  wsl_list = `wsl --list --verbose`
-  m = /^\*?\s+#{distro}\s+(\w+)\s+(\d)$/.match(wsl_list)
-  if m.nil?
-    Open3.popen2('wsl', '--install', distro) do |stdin, stdout, _thr|
-      sleep 10
-      puts 'wsl...'
-      buff = String.new
-      loop do
-        unless stdout.read_nonblock(1, buff)
-          puts 'wait...'
-          sleep 1
-          next
-        end
-        pp buff
-        case buff
-        when /Enter new UNIX username:/
-          buff.clear
-          stdin.puts(user)
-          stdin.flush
-        when /New password:/, /Retype new password:/
-          buff.clear
-          stdin.puts(password)
-          stdin.flush
-        when /\[[^\]]*\]\$/
-          buff.clear
-          stdin.puts('exit')
-          stdin.flush
-        when /logout/
-          stdin.close
-          stdout.close
-          break
-        end
-      end
-    end
-  end
-end
-
 distro = 'OracleLinux_9_1'
-install_wsl(distro, user: 'whs', password: 'whs')
 
-__END__
-
-@ECHO OFF
-SETLOCAL
-SET DISTRO=OracleLinux_9_1
-SET WSL_LIST="%~dp0\wsl_list.txt"
-
-wsl --list --verbose > %WSL_LIST%
-
-FINDSTR "%DISTRO%.*1$" %WSL_LIST%
-IF %ERRORLEVEL% EQU 0 GOTO SKIP_SET_VERSION
-
-FINDSTR "%DISTRO%" %WSL_LIST%
-IF %ERRORLEVEL% EQU 0 GOTO SKIP_INSTALL
-
-ECHO ---- Intall Linux ----
-wsl --install %DISTRO%
-IF %ERRORLEVEL% NEQ 0 GOTO ERR
-:SKIP_INSTALL
-
-echo ---- Set WSL versinon 1 ----
-wsl --set-version %DISTRO% 1
-IF %ERRORLEVEL% NEQ 0 GOTO ERR
-:SKIP_SET_VERSION
-
-echo ---- Setup ----
-wsl --distribution  %DISTRO% ./setup.sh
-IF %ERRORLEVEL% NEQ 0 GOTO ERR
-
-GOTO :EOF
-
-:ERR
-ECHO.
-ECHO Error has occurred!
-PAUSE
-
-ENDLOCAL
+wsl_list = `wsl --list --verbose`.encode('UTF-8', 'UTF-16LE')
+m = /^\*?\s+#{distro}\s+\w+\s+(\d)$/.match(wsl_list)
+system("wsl --install #{distro}") if m.nil?
+system("wsl --set-version #{distro} 1") if m.nil? || m[1] != '1'
+system("wsl --distribution #{distro} --cd \"#{__dir__}\" -- ./setup.sh")
