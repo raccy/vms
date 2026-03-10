9543ca60a2a5a1c555097e96cd96e75113a5946c
IGARASHI Makoto <igarashi@kyokyo-u.ac.jp>
2026-03-10 11:53:06 +0900

    setup.cmdが動くようにする。

--- wsl/setup.cmd
+++ wsl/setup.cmd
@@ -1,3 +1,6 @@
 @echo off
-set RAKE="rake.bat"
-%RAKE% -f "%~dp0Rakefile" %*
+@setlocal
+set RUBY_BIN_DIR=
+for /f "delims=" %%i in ('where.exe ruby.exe 2^>nul') do set "RUBY_BIN_DIR=%%~dpi"
+%RUBY_BIN_DIR%ruby.exe %RUBY_BIN_DIR%rake -f "%~dp0Rakefile" %*
+@endlocal
